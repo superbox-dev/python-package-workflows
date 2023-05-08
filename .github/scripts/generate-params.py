a7da566844d86558f511b09c2048ba15965d6f97
Michael Hacker <mh@superbox.one>
2023-05-08 21:11:25 +0200

    Test python script

--- .github/scripts/generate-params.py
+++ .github/scripts/generate-params.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import argparse
 import subprocess
 import re
 import json
@@ -7,44 +7,63 @@ import json
 from configparser import ConfigParser
 from os import environ
 from pathlib import Path
+from typing import Any
+from typing import Dict
+from typing import List
 
-parameters: dict = json.loads(Path("${{ inputs.json }}").read_text())
-python_versions: list = parameters.get("python-versions", ["3.11"])
-binary_files: list = []
 
-cf: ConfigParser = ConfigParser()
-cf.read("setup.cfg")
+def main(args: argparse.Namespace) -> None:
+    parameters: Dict[str, Any] = json.loads(Path(args.INPUTS_JSON).read_text())
+    python_versions: List[str] = parameters.get("python-versions", ["3.11"])
+    binary_files: List[str] = []
 
-if "options.entry_points" in cf.sections():
-  for item in cf["options.entry_points"]["console_scripts"].split("\n"):
-      if item:
-          binary_files.append(item.split("=")[0].strip())
+    cf: ConfigParser = ConfigParser()
+    cf.read("setup.cfg")
 
-latest_python_version: str = python_versions[-1]
-version_dev: bool = False
+    if "options.entry_points" in cf.sections():
+        for item in cf["options.entry_points"]["console_scripts"].split("\n"):
+            if item:
+                binary_files.append(item.split("=")[0].strip())
 
-if re.match(r"\d{4}\.\d{1,2}\.dev.*", "${{ github.ref_name }}"):
-  version_dev = True
+    latest_python_version: str = python_versions[-1]
+    version_dev: bool = False
 
-result_package_name: subprocess.CompletedProcess = subprocess.run("python setup.py --name", shell=True, capture_output=True)
-package_name: str = result_package_name.stdout.decode().strip()
+    if re.match(r"\d{4}\.\d{1,2}\.dev.*", args.GITHUB_REF_NAME):
+        version_dev = True
 
-result_package_version: subprocess.CompletedProcess = subprocess.run("python setup.py --version", shell=True, capture_output=True)
-package_version: str = result_package_version.stdout.decode().strip()
+    result_package_name: subprocess.CompletedProcess = subprocess.run(
+        "python setup.py --name", shell=True, capture_output=True
+    )
+    package_name: str = result_package_name.stdout.decode().strip()
 
-if version_dev:
-  package_version += ".dev${{ github.run_number }}"
+    result_package_version: subprocess.CompletedProcess = subprocess.run(
+        "python setup.py --version", shell=True, capture_output=True
+    )
+    package_version: str = result_package_version.stdout.decode().strip()
 
-github_output: Path = Path(environ["GITHUB_OUTPUT"])
+    if version_dev:
+        package_version += f".dev{args.GITHUB_RUN_NUMBER}"
 
-with github_output.open("a", encoding="utf-8") as gho:
-  gho.write(
-      f"python-versions={python_versions!s}\n"
-      f"latest-python-version={latest_python_version!s}\n"
-      f"package-name={package_name!s}\n"
-      f"package-version={package_version!s}\n"
-      f"binary-files={binary_files!s}\n"
-      f"version-dev={version_dev!s}\n"
-  )
+    github_output: Path = Path(environ["GITHUB_OUTPUT"])
 
-print(github_output.read_text())
+    with github_output.open("a", encoding="utf-8") as gho:
+        gho.write(
+            f"python-versions={python_versions!s}\n"
+            f"latest-python-version={latest_python_version!s}\n"
+            f"package-name={package_name!s}\n"
+            f"package-version={package_version!s}\n"
+            f"binary-files={binary_files!s}\n"
+            f"version-dev={version_dev!s}\n"
+        )
+
+    print(github_output.read_text())
+
+
+if __name__ == "__main__":
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument("INPUTS_JSON")
+    parser.add_argument("GITHUB_REF_NAME")
+    parser.add_argument("GITHUB_RUN_NUMBER")
+    args: argparse.Namespace = parser.parse_args()
+
+    main(args)
