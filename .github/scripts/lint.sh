1046894305679a7d42356cfad3c91316dd3ccc35
Michael Hacker <mh@superbox.one>
2023-06-25 20:54:31 +0200

    Fix shellcheck and add shellcheck to lint.sh

--- .github/scripts/lint.sh
+++ .github/scripts/lint.sh
@@ -24,3 +24,8 @@ yamllint --version
 # shellcheck disable=SC2046
 yamllint $(git ls-files '*.yaml') $(git ls-files '*.yml')
 
+sudo -E apt-get -qq update
+sudo -E apt-get -qq upgrade
+sudo apt install shellcheck
+
+find . -type f -name "*.sh" -exec shellcheck --format=gcc {} \;
