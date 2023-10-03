0113af08b571668ff211704a92808b38fe5d04a3
Michael Hacker <mh@superbox.one>
2023-10-03 04:14:16 +0200

    Removed pylint

--- .github/scripts/lint.sh
+++ .github/scripts/lint.sh
@@ -16,10 +16,6 @@ ruff --version
 # shellcheck disable=SC2046
 ruff $(git ls-files '*.py')
 
-pylint --version
-# shellcheck disable=SC2046
-pylint $(git ls-files '*.py')
-
 yamllint --version
 # shellcheck disable=SC2046
 yamllint $(git ls-files '*.yaml') $(git ls-files '*.yml')
